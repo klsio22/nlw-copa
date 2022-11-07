082f3acd95958f80af6b4b0bea5185b82a96ebd6
Klesio Nascimento <klesio.dev@gmail.com>
2022-11-06 23:04:34 -0300

    Added generators in backend and frontend with next js.

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,17 +1,31 @@
+import { PrismaClient } from '@prisma/client';
+import cors from '@fastify/cors'
 import Fastify from 'fastify';
+
+const prisma = new PrismaClient({
+  log: ['query']
+})
 
 async function bootstrap() {
   const fastify = Fastify({
     logger: true,
   });
 
+  await fastify.register(cors, {
+    origin: true,
+  })
+
+  fastify.get('/pools/count', async () => {
+    const count = await prisma.pool.count()
+    return { count }
+  })
+
+  /*   fastify.get('/pools/count', () => {
+      return { count: 423423 };
+    }); */
+
   // http://localhost:3333/pools/count
-
-  fastify.get('/pools/count', () => {
-    return { count: 993213339 };
-  });
-
-  await fastify.listen({ port: 3333 });
+  await fastify.listen({ port: 3333, host: '0.0.0.0' });
 }
 
 bootstrap();
